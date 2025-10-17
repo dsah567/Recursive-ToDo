c01234894dc9f65e57eacb261cec6925a895569a
Dipu Sah <sahd7929@gmail.com>
2025-10-17 17:16:02 +0530

    features: add handlesavetodo,handlechangetodo,Todo
    handlesavetodo for updating a todo
    handlechangetodo for changing completed option of todo
    todo for each todos to have checkbox, save/edit option

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -32,6 +32,54 @@ export default function TodoList() {
     }
   }
 
+  /**
+   * 
+   * @param id of the todo
+   * @param todo new todo 
+   * replace the newtodo in same place of id's todo
+   */
+  function handleSaveTodo(id: string, todo: ToDo) {
+      if (todoList != null) {
+        const newTodoList: ToDo[] = todoList.map((t) => {
+          if (t.id === id) {
+            return todo;
+          } else {
+            return t;
+          }
+      })
+      
+      setTodoList(newTodoList);
+      localStorage.setItem("todoSubTodo",JSON.stringify(newTodoList));
+    }
+  }
+
+  /**
+   * 
+   * @param id of the todo which completed option want to change
+   * and then update the todolist and localstorage
+   */
+  function handleChangeTodo(id: string) {
+    if (todoList != null) {
+      const newTodoList: ToDo[] = todoList.map((t) => {
+        if (t.id === id) {
+          if (t.completed === true) {
+            t.completed = false;
+            return t;
+          } else {
+              //later write code check if all of its subtodo is completed
+              t.completed = true; 
+            return t;
+          }
+        } else {
+          return t;
+        }
+    })
+    
+    setTodoList(newTodoList);
+    localStorage.setItem("todoSubTodo",JSON.stringify(newTodoList));
+  }
+  }
+
   if(todoList === null) {
     return(
       <div>
@@ -54,7 +102,7 @@ export default function TodoList() {
             {
               todoList.map((todos: ToDo) => (
                 <li key={todos.id}>
-                  {todos.name}
+                  <Todo todo={todos} handleSaveTodo={handleSaveTodo} handleChangeTodo={handleChangeTodo}/>
 
                   <button
                   onClick={() => handleDelteTodo(todos.id)}
@@ -69,4 +117,53 @@ export default function TodoList() {
       </div>
     )
   }
+}
+
+
+  /**
+   * function to display each todo of todolist
+   * @param param0 todo
+   * @param param1 handlesavetodo for adding new todo in same place of previous todo
+   * @param param2 handleChangeTodo for changing the completedoption of todo
+   * @returns todo with edit/save and checkbox 
+   */
+  function Todo({todo,handleSaveTodo, handleChangeTodo}: {todo: ToDo,handleSaveTodo: (id: string, todo: ToDo) => void, handleChangeTodo: (id: string) => void}) {
+    const [isEditing, setIsEditing] = useState<boolean>(false);
+    const [todoName, setTodoName] = useState<string>(todo.name);
+
+    function handleSave() {
+      const tempTodo: ToDo = {
+       id: todo.id,
+       name: todoName,
+       description: todo.description,
+       completed: todo.completed,
+       SubToDo: todo.SubToDo
+      }
+      handleSaveTodo(todo.id, tempTodo);
+      setIsEditing(false);
+    }
+
+    if (isEditing) {
+      return (
+      <>
+        <input type="text" 
+          value={todoName}
+          onChange={(e) => setTodoName(e.target.value)}/> 
+
+          <button onClick={() => handleSave()}>Save</button>
+      </>)
+
+    } else {
+        return (<>
+        <input 
+        type="checkbox"
+        checked={todo.completed} 
+        onChange={() => handleChangeTodo(todo.id)}
+        />
+        
+        {todo.name} 
+        
+        <button onClick={() => setIsEditing(true)}>Edit</button>
+        </>)
+    }
 }
