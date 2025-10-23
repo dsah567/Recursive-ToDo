1f6915899d57602ed0463e7eb050bd956e9cd92c
Dipu Sah <sahd7929@gmail.com>
2025-10-23 18:10:20 +0530

    change title to Recursive ToD0

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ return (
     <div className="m-4">
       <div className="text-center">
         <span className="bg-[#8440ac] m-2 p-2 rounded">
-          ToDo - SubToDo
+          Recursive ToDo
         </span>
       </div>
       <div className="m-2" >
